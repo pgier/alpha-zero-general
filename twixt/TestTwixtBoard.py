32ccfa3d67d191e5d73facdbd7e19bafe18e765b
Paul Gier <paulgier@gmail.com>
2022-12-28 20:07:52 -0600

    twixt: rename test files

--- twixt/TestTwixtBoard.py
+++ twixt/TestTwixtBoard.py
@@ -10,8 +10,8 @@ class TestTwixtBoard(unittest.TestCase):
     def setUp(self):
         self.board = TwixtBoard(TEST_BOARD_SIZE)
 
-    def test_hasLegalMoves(self):
-        self.assertTrue(True, self.board.has_legal_moves())
+    def testHasLegalMoves(self):
+        self.assertTrue(self.board.has_legal_moves())
 
 
 if __name__ == '__main__':
--- twixt/TestTwixtGame.py
+++ twixt/TestTwixtGame.py
@@ -9,11 +9,11 @@ class TestTwixtBoard(unittest.TestCase):
     def setUp(self):
         self.game = TwixtGame(TEST_BOARD_SIZE)
 
-    def test_getBoardSize(self):
+    def testGetBoardSize(self):
         currentBoardSize = self.game.getBoardSize()
         self.assertEqual(TEST_BOARD_SIZE, currentBoardSize)
 
-    def test_getInitBoard(self):
+    def testGetInitBoard(self):
         initialBoard = self.game.getInitBoard()
         self.assertIsNotNone(initialBoard)
 
